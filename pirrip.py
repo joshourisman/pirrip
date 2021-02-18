aabda6a3f898b96b172e5bc6ecebc0b6ae80a892
Josh Ourisman <me@josho.io>
2021-02-18 23:12:54 +0000

    Stubbed out response for simple PyPI API endpoint

--- pirrip.py
+++ pirrip.py
@@ -1,7 +1,11 @@
 import requests
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/pypi/{package_name}/json")
@@ -12,3 +16,10 @@ async def package_info(package_name: str):
 @app.get("/pypi/{package_name}/{release}/json")
 async def release_info(package_name: str, release: str):
     return requests.get(f"https://pypi.org/pypi/{package_name}/{release}/json").json()
+
+
+@app.get("/simple/{package_name}", response_class=HTMLResponse)
+async def read_item(request: Request, package_name: str):
+    return templates.TemplateResponse(
+        "package.html", {"request": request, "package_name": package_name}
+    )
