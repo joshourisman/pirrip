1b30439614600e54a4ca0b6532013681f350c5d6
Josh Ourisman <me@josho.io>
2021-03-04 22:09:47 +0000

    Added package_list() view that lists all packages in PACKAGE_DIR.

--- pirrip.py
+++ pirrip.py
@@ -27,6 +27,17 @@ async def release_info(package_name: str, release: str):
     return requests.get(f"https://pypi.org/pypi/{package_name}/{release}/json").json()
 
 
+@app.get("/simple", response_class=HTMLResponse)
+async def list_packages(request: Request):
+    settings = PirripSettings()
+    package_dir = settings.PACKAGE_DIR
+    packages = [obj for obj in package_dir.iterdir() if obj.is_dir() is True]
+
+    return templates.TemplateResponse(
+        "package_list.html", {"request": request, "packages": packages}
+    )
+
+
 @app.get("/simple/{package_name}", response_class=HTMLResponse)
 async def read_item(request: Request, package_name: str):
     return templates.TemplateResponse(
